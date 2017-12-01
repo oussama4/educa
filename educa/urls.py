a44400ac45e5bc339aa680467d441e1ec7ebdd4b
oussama4 <oussamazouaki4@gmail.com>
2017-12-01 16:00:28 +0000

    add the authentication system

--- educa/urls.py
+++ educa/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^accounts/login/$', auth_views.LoginView.as_view, name='login'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
