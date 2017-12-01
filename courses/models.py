5413539629e2241c2e3d955c7cc74eaa910dbbeb
oussama4 <oussamazouaki4@gmail.com>
2017-12-01 13:45:29 +0000

    create a custom model field

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+from .fields import OrderField
+
 class Subject(models.Model):
     """
     subject of the courses
@@ -41,9 +43,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules')
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
-        return self.title
+        return '{}. {}'.format(self.order, self.title)
 
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name='contents')
@@ -54,6 +60,10 @@ class Content(models.Model):
                         limit_choices_to={'model__in': ('text', 'video', 'image', 'file')})
     object_id = models.PositiveIntegerField() # primary key of the related model
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(User, related_name='%(class)s_related')
