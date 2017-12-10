137dc18a6862b03bee6a04419a0fa9add9da83dd
oussama4 <oussamazouaki4@gmail.com>
2017-12-10 22:10:36 +0000

    create view sets and routers

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from ..models import Subject, Course
-from .serializers import SubjectSerializer
+from .serializers import SubjectSerializer, CourseSerializer
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -24,3 +24,7 @@ class CourseEnrollView(APIView):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
