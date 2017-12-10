71de7b90582fa0564255255e6fd2784b015e656d
oussama4 <oussamazouaki4@gmail.com>
2017-12-10 16:55:03 +0000

    caching views

--- students/urls.py
+++ students/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 
 from . import views
 
@@ -8,8 +9,10 @@ urlpatterns = [
         name='student_enroll_course'),
     url(r'^courses/$', views.StudentCourseListView.as_view(),
         name='student_course_list'),
-    url(r'^courses/(?P<pk>\d+)/$', views.StudentCourseDetailView.as_view(),
+    url(r'^courses/(?P<pk>\d+)/$', 
+        cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
         name='student_course_detail'),
-    url(r'^courses/(?P<pk>\d+)/(?P<module_id>\d+)/', views.StudentCourseDetailView.as_view(),
+    url(r'^courses/(?P<pk>\d+)/(?P<module_id>\d+)/', 
+        cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
         name='student_course_detail_module'),
 ]
