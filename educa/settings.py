aa87ab6563b27fb34db16193266b019428788804
oussama4 <oussamazouaki4@gmail.com>
2017-12-10 17:51:44 +0000

    setup django rest framework

--- educa/settings.py
+++ educa/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'embed_video',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -138,4 +139,13 @@ CACHES = {
         'LOCATION': '127.0.0.1:11211',
         'TIMEOUT': 2400,
     }
+}
+
+# rest framework
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
